43f68dc3d3203755b05f4c387b990e753f832040
Yoann Masson <yoann.masson95@gmail.com>
2017-02-15 11:21:46 +0100

    séance 15/02 ajout gestion erreur id

--- IOSProject/DemandeInscriptionViewController.swift
+++ IOSProject/DemandeInscriptionViewController.swift
@@ -21,15 +21,26 @@ class DemandeInscriptionViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func valider(_ sender: Any) {
-        
-    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func valider(_ sender: Any) {
+        guard let text = self.identifiant.text, text != "" else{
+            self.alertError(errorMsg: "Problème d'Id", userInfo: "Il faut renseigner l'identifiant qui vous permettra de vous connecter")
+            return
+        }
+    }
+        
+    func alertError(errorMsg error: String, userInfo msg:String = ""){
+        let alert = UIAlertController (title: error,
+                                       message: msg,
+                                       preferredStyle:.alert)
+        let cancelAction = UIAlertAction (title :"Ok", style:.default)
+        alert.addAction(cancelAction)
+        present(alert,animated: true)
+    }
     /*
     // MARK: - Navigation
 
