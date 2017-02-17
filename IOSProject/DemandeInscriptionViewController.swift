3a69ba518f1fd7e83b26df87cfb53c01b1cb9bca
Yoann MASSON <yoann.masson@igtp4-mac08.isim.intra>
2017-02-17 15:06:51 +0100

    Ajout transition et écran du choix du groupe

--- IOSProject/DemandeInscriptionViewController.swift
+++ IOSProject/DemandeInscriptionViewController.swift
@@ -15,6 +15,14 @@ class DemandeInscriptionViewController: UIViewController {
     @IBOutlet weak var prenom: UITextField!
     @IBOutlet weak var mdp: UITextField!
     @IBOutlet weak var confirmationmdp: UITextField!
+    @IBOutlet weak var valider: UIButton!
+    @IBOutlet weak var topLabel: UILabel!
+    
+    let transitionEtud = "transitionEtud"
+    let transitionEnseignant = "transitionEnseignant"
+    let transitionRespo = "transitionRespo"
+    let transitionAdmin = "transitionAdmin"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,9 +35,45 @@ class DemandeInscriptionViewController: UIViewController {
     }
     
     @IBAction func valider(_ sender: Any) {
-        guard let text = self.identifiant.text, text != "" else{
+        /** Testing that all the fiels are correctly filled */
+        guard let testid = self.identifiant.text, testid != "" else{
             self.alertError(errorMsg: "Problème d'Id", userInfo: "Il faut renseigner l'identifiant qui vous permettra de vous connecter")
             return
+        }
+        guard let testnom = self.nom.text, testnom != "" else{
+            self.alertError(errorMsg: "Problème de nom", userInfo: "Il faut renseigner votre nom dans le champ \"Nom\"")
+            return
+        }
+        guard let testprenom = self.prenom.text, testprenom != "" else{
+            self.alertError(errorMsg: "Problème de prenom", userInfo: "Il faut renseigner votre prénom dans le champ \"Prénom\"")
+            return
+        }
+        guard let testmdp = self.mdp.text, testmdp != "" else{
+            self.alertError(errorMsg: "Problème de mdp", userInfo: "Il faut renseigner le mot de passe que vous souhaitez utiliser")
+            return
+        }
+        
+        guard let testconfirmationmdp = self.confirmationmdp.text, testconfirmationmdp == self.mdp.text else{
+            self.alertError(errorMsg: "Problème de confirmation", userInfo: "Merci de confirmer le mot de passe que vous souhaitez utiliser")
+            return
+        }
+    }
+    
+    
+    
+    override func prepare (for segue: UIStoryboardSegue, sender: Any?){
+        switch segue.identifier! {
+        case self.transitionEtud:
+                self.topLabel.text! = "Demande inscription: Etudiants"
+        case self.transitionAdmin:
+                self.topLabel.text! = "Demande inscription: Administration"
+        case self.transitionRespo:
+                self.topLabel.text! = "Demande inscription: Responsable dep."
+        case self.transitionEnseignant:
+                self.topLabel.text! = "Demande inscription: Enseignant"
+        default:
+             alertError(errorMsg: "Problème de groupe", userInfo: "L'application ne reconnaît pas votre groupe")
+            
         }
     }
         
@@ -40,6 +84,11 @@ class DemandeInscriptionViewController: UIViewController {
         let cancelAction = UIAlertAction (title :"Ok", style:.default)
         alert.addAction(cancelAction)
         present(alert,animated: true)
+    }
+    
+    func textFieldShouldReturn ( textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
     /*
     // MARK: - Navigation
