0231096cfcda446cec8ab71e2d8c7f93547f0b4d
yoannMasson <yoann.masson95@gmail.com>
2017-03-22 09:34:52 +0100

    ajout doc

---
+++ IOSProject/DocumentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DocumentTableViewCell.swift
+//  IOSProject
+//
+//  Created by Yoann MASSON on 22/03/2017.
+//  Copyright © 2017 Yoann MASSON. All rights reserved.
+//
+
+import UIKit
+
+class DocumentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
